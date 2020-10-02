9d553d53c7ae9e8714a5b4720da9c0643e70432e
Habib <habibullo.boss@gmail.com>
2020-10-02 23:12:04 +0500

    Added types

---
+++ pkg/types/types.go
@@ -0,0 +1,36 @@
+package types
+
+
+// Money представляет собой в минимальных единицах (центы, копейки, дирамы и т.д.)
+type Money int64
+
+// PaymentCategory представляет собой категорию, в каторой был совершён платёж (авто, аптеки, рестораны и т.д.).
+type PaymentCategory string
+
+// PaymentStatus представляет собой статус платёжа
+type PaymentStatus string
+
+
+const (
+  PaymentStatusOk PaymentStatus = "OK"
+  PaymentStatusFail PaymentStatus = "FAIL"
+  PaymentStatusInProgress PaymentStatus = "INPROGREES"
+)
+
+//Payment представляет информацию о платеже
+type Payment struct {
+  ID 			string
+  AccountID 	int64
+  Amount 		Money
+  Category		PaymentCategory
+  Status		PaymentStatus
+}
+
+type Phone string
+
+type Account struct {
+  ID     	int64
+  Phone  	Phone
+  Balance 	Money
+}
+
